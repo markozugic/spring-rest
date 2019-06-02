c28a98b68379b198967db0b9ed3d526370badff3
marko.zugic <markozugic4@gmail.com>
2019-06-02 19:31:01 +0200

    first commit

---
+++ src/main/java/payroll/PayrollAplication.java
@@ -0,0 +1,13 @@
+package payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollAplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollAplication.class, args);
+	}
+
+}
